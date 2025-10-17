f7aa6614e7c1f5eefec12158e27bda28fe526e5b
kolown2007 <kolown@gmail.com>
2025-10-17 16:54:33 +0800

    edit

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : (process.env.BASE_PATH || '/cyber_purok')
 		}
 	}
 };

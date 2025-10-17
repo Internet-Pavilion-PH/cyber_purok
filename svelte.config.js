15bbefa23503bc1d1c64a69e7be0df3be9d5073b
kolown2007 <kolown@gmail.com>
2025-10-17 16:40:26 +0800

    update

--- svelte.config.js
+++ svelte.config.js
@@ -4,15 +4,11 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'docs',
-			assets: 'docs',
-			// produce an SPA fallback so dynamic client-side routes work
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
